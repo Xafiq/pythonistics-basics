0b9d53704dd6ed79c0c6549f0c6f0491ebe22f78
Xafiq <148711588+Xafiq@users.noreply.github.com>
2024-09-18 16:15:21 +0200

    Add files via upload

---
+++ lists/retrieve-an-element-from-a-list/script.py
@@ -0,0 +1,10 @@
+my_list = ["Python", "C++", "Java"]
+my_list[0]  # "Python"
+my_list[1]  # "C++"
+my_list[2]  # "Java"
+my_list[-1]  # "Java"
+my_list[-2]  # "C++"
+my_list[-3]  # "Python"
+my_list[-4]  # Error! Index out of range
+my_list[3]  # Error! Index out of range
+
