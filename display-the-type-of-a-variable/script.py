fd1a4358134aba77034610804df7188719528ad0
Xafiq <148711588+Xafiq@users.noreply.github.com>
2024-09-18 15:50:36 +0200

    Add files via upload

---
+++ display-the-type-of-a-variable/script.py
@@ -0,0 +1,7 @@
+number = input("Enter a number: ")
+print(type(number))  # returns <class 'str'>. The input function always returns a string.
+
+# You can then convert the variable 'number' to an integer using the int function
+number = int(number)
+print(type(number))  # returns <class 'int'>. The variable has been successfully converted.
+
